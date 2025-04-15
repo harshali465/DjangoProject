d3e121e6d5463daf273c8046a706aaa43c5a994e
harshali465 <patilharshali465@gmail.com>
2025-04-15 15:26:09 +0530

    Update models

--- django1/models.py
+++ django1/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Task(models.Model):
     Name = models.CharField(max_length=200)
     Post = models.CharField(max_length=300)
-    Contact = models.CharField(max_length=10)
+    Contact = models.IntegerField()
     Email = models.EmailField(max_length=232)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
